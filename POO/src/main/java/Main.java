2063bd4db0e417394c7da9bfe93969e0abd3dd8b
david <71474892+kiritokun123-xd@users.noreply.github.com>
2022-08-07 11:25:46 -0500

    Update Main.java
    borrado de comentarios

--- POO/src/main/java/Main.java
+++ POO/src/main/java/Main.java
@@ -1,33 +1,7 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
-        /*Scanner scan = new Scanner(System.in);
-
-        System.out.println("Digite el nombre del perro");
-        String nombrePerro = scan.nextLine();
-
-        Perro perro1 = new Perro(nombrePerro);
-        perro1.setEdad(2);
-        perro1.setColor("Blanco");
-        perro1.setNumeroPatas(4);
-        perro1.setRaza("Salchicha");
-        perro1.isVacunado(false);
-
-        //Se accede a los atributos por los metodos get
-        System.out.println("El perro " + perro1.getNombre() + " tiene " + perro1.getEdad() + " años.");*/
-        //String abc = "Bienvenido al mundo de Java. Tu mundo nuevo";
-        //System.out.println(abc.substring(abc.indexOf("m"),18));
-
-        /*int lista[] = {12,10,10,9};
-        double promedio = 0;
-
-        for (int i= 0;i< lista.length;i++){
-            promedio = promedio + lista[i];
-        }
-
-        promedio = promedio / lista.length;
-        promedio = Math.round(promedio*100.00)/100.0;
-        System.out.println(promedio);*/
+        
         int n = 5; int i; int j;
         for (i=1;i<=n;i++){
         }
