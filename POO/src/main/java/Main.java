97a346794e1665fcd408e3a82f8c1b51634d66e3
kiritokun123-xd <david22android@gmail.com>
2022-08-07 11:28:50 -0500

    cambios en comentarios saludo

--- POO/src/main/java/Main.java
+++ POO/src/main/java/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         //hola a todos
-        //comentar cosas importantes
+        //comentar cosas importantess
         int n = 5; int i; int j;
         for (i=1;i<=n;i++){
         }
